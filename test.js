62b36f67cabd87afa164fa078e86618347dce129
anastasiiapo <anastasiiapopova366@gmail.com>
2023-10-19 17:40:37 -0300

    test1

--- test.js
+++ test.js
@@ -16,6 +16,7 @@ const wdOpts = {
 const appEco_path = '~Ecoplatform DEV';
 const btn_path = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.Button';
 const dev = 'exp://u.expo.dev/update/1948fed0-461c-4ee5-8eeb-8272910d266d';
+const connect = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup/android.view.ViewGroup[4]/android.view.ViewGroup/android.widget.Button';
 
 async function runTest() {
   const driver = await remote(wdOpts);
@@ -24,8 +25,6 @@ async function runTest() {
 //  console.log(btn.innerHTML);
 //  await driver.pause(10000);
   await appEco.click();
-  await driver.pause(5000);
-
   const btn = await driver.$(btn_path);
   await btn.click();
   await driver.pause(5000);
@@ -33,7 +32,12 @@ async function runTest() {
 
 
 //    const batteryItem = await driver.$('#DevLauncherURLInput');
-    const batteryItem = await driver.$("//*[@class='android.widget.EditText']").addValue(dev);
+    const link = await driver.$("//*[@class='android.widget.EditText']").addValue(dev);
+    driver.pressKeyCode(66);
+    const launch = await driver.$(connect);
+    await launch.click();
+    await driver.pause(10000);
+
 
   } finally {
 //    await driver.deleteSession();
